bda1ca6772beabef68870a9c01fe1d49eed0c1e3
ir <Isabelaruiz147@gmail.com>
2024-09-02 13:16:44 -0500

    Initial Commit

---
+++ Ur Parking/Ur_ParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ur_ParkingApp.swift
+//  Ur Parking
+//
+//  Created by Isabela Ruiz Bustos on 2/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Ur_ParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
